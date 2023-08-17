1b6b5010c15d06d50aaca5ebf8c27ecb4c682e6f
Jossean Rivera <jossean.rivera@outlook.com>
2023-08-16 20:21:28 -0700

    Adding font awesome and style dark mode button

--- src/react-myblog/src/components/layout/Footer.js
+++ src/react-myblog/src/components/layout/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { getDarkModeFlag, toggleDarkMode } from '../../state/themeSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function Footer() {
     const dispatch = useDispatch();
@@ -12,10 +14,10 @@ export default function Footer() {
             {
                 darkModeFlag ?
                     <Button variant="outline-light" onClick={() => dispatch(toggleDarkMode())}>
-                        Light Mode
+                        <FontAwesomeIcon icon={icon({ name: 'sun' })} /> Switch to Light Mode
                     </Button> :
                     <Button variant="outline-dark" onClick={() => dispatch(toggleDarkMode())}>
-                        Dark Mode
+                        <FontAwesomeIcon icon={icon({ name: 'moon' })} /> Switch to Dark Mode
                     </Button>
             }
         </div>
